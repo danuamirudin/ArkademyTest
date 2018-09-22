aaf6b7889565a7e333ec22d75aa1057989a356f0
danuamirudin <danuamirudin@outlook.com>
2018-09-22 21:12:08 +0700

    Tiada perjuangan yang sia-sia di mata Allah

--- 2/tutpython/registration/forms.py
+++ 2/tutpython/registration/forms.py
@@ -7,13 +7,14 @@ class RegistrationForm(UserCreationForm):
  
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email',' phone_number','password1', 'password2')
  
     def save(self, commit=True):
         user = super(RegistrationForm, self).save(commit=False)
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
+        user.phone_number = self.cleaned_data['phone_number']
         
  
         if commit:
